f4d31321ee8a87ec5b56cbc0cb5159ea76592877
Tegor International <104412526+Tegorinternational@users.noreply.github.com>
2022-09-22 21:52:47 +0530

    Add files via upload

---
+++ js/fetch_date.jsx
@@ -0,0 +1,35 @@
+class LatestCommitComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "",
+      date: ""
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://api.github.com/repos/tegorinternational/GitHub.io/branches/master"
+    )
+      .then(response => {
+        response.json().then(json => {
+          console.log(json);
+          this.setState({
+            author: json.commit.author.login,
+            date: json.commit.commit.author.date
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div class="text-base font-light text-gray-600">{this.state.author}</div>
+        <div class="text-2xl font-medium text-blue-600">{this.state.date}</div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<LatestCommitComponent />, document.getElementById("updated_date"));
